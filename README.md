bbd363f127a063ddc7b4cea29230bcc1475dd82d
user <user@users-MacBook-Pro-2.local>
2021-06-23 15:44:09 -0700

    max bike repair server complete

---
+++ README.md
@@ -0,0 +1,44 @@
+## Max Bike Repair
+
+
+### About This Site:
+
+- This is a Responsive MERN-Stack web applications for motorcycle parts repair.
+- In this site user can take our service, see his order list & reviews our site. 
+- If user want any services he must be authenticate with gmail or google. 
+- User can also cancelled his order. But he/she can't cancel his first order. If he/she want to cancel his order he need to order more then one services. 
+- There is a admin panel where admin can add more admins,  add more services,  update order condition of users.
+
+### Front-End Technology:
+
+- React.JS
+- Javascript
+- React Router Dom
+- Firebase Authentication
+- React Hook From
+- React Bootstrap 
+- HTML5
+- CSS3
+
+### Back-End Technology:
+
+- Node.Js
+- Express.JS
+- MongoDB
+
+### Tools:
+- VS CODE
+- Git
+- GitHub
+- Dotenv
+
+## Screenshots
+![](screenShot/s1.PNG)
+![](screenShot/s2.PNG)
+![](screenShot/s3.PNG)
+![](screenShot/s4.PNG)
+![](screenShot/s5.PNG)
+![](screenShot/s6.PNG)
+![](screenShot/s7.PNG)
+![](screenShot/s8.PNG)
+![](screenShot/s9.PNG)
---
+++ index.js
@@ -0,0 +1,181 @@
+const express = require("express");
+
+const port = 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs-extra");
+const fileUpload = require("express-fileupload");
+const { ObjectId } = require("mongodb");
+require("dotenv").config();
+
+console.log(process.env.DB_USER)
+
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ibkg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+/* app.use(express.static("services")); */
+app.use(fileUpload());
+
+// default / root
+app.get("/", function (req, res) {
+  res.send("Hi I'm Saikat Hossain!!!");
+});
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const serviceCollection = client
+    .db(process.env.DB_NAME)
+    .collection("services");
+  const feedbackCollection = client
+    .db(process.env.DB_NAME)
+    .collection("UserReviews");
+  const OrderCollection = client.db(process.env.DB_NAME).collection("orders");
+  const AddAdminCollection = client
+    .db(process.env.DB_NAME)
+    .collection("admins");
+
+  // create and add new service part
+
+  app.post("/addServices", (req, res) => {
+    const file = req.files.file;
+    const serviceData = req.body;
+    const fileData = file.data;
+    const encFile = fileData.toString("base64");
+
+    const convertedFile = {
+      contentType: file.mimetype,
+      size: parseFloat(file.size),
+      img: Buffer.from(encFile, "base64"),
+    };
+    const FinalData = {
+      title: serviceData.title,
+      description: serviceData.description,
+      file: convertedFile,
+    };
+
+    serviceCollection
+      .insertOne(FinalData)
+      .then((result) => {
+        if (result.insertedCount > 0) {
+          res.sendStatus(200);
+        }
+      })
+      .catch((err) => console.log(err));
+  });
+
+  //service data
+  app.get("/services", (req, res) => {
+    serviceCollection
+      .find({})
+      .limit(6)
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  //User feedback load to  database
+  app.post("/addFeedback", (req, res) => {
+    const feedback = req.body;
+
+    console.log(feedback);
+    feedbackCollection.insertOne(feedback).then((result) => {
+      console.log(result);
+      res.send(result);
+    });
+  });
+
+  app.get("/feedback", (req, res) => {
+    feedbackCollection
+      .find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+        if (err) {
+          console.log(err);
+        }
+      });
+  });  // /* .limit(6) */ 
+
+  //
+  //add orders by customer
+  // for add order
+  app.post("/addOrder", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const title = req.body.title;
+    const file = req.files.file;
+    const details = req.body.details;
+    const price = req.body.price;
+    const newImg = file.data;
+    const encImg = newImg.toString("base64");
+    var image = {
+      contentType: req.files.file.mimetype,
+      size: req.files.file.size,
+      img: Buffer.from(encImg, "base64"),
+    };
+    OrderCollection.insertOne({
+      name,
+      email,
+      title,
+      details,
+      price,
+      image,
+    }).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/orders", (req, res) => {
+    OrderCollection.find({}).toArray((error, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get("/orderedList", (req, res) => {
+    OrderCollection.find({ email: req.query.email }).toArray(
+      (error, documents) => {
+        res.send(documents);
+      }
+    );
+  });
+  //admin added
+
+  //add admins
+  app.post("/addAdmin", (req, res) => {
+    const admin = req.body;
+    AddAdminCollection.insertOne(admin)
+      .then((result) => {
+        if (result.insertedCount > 0) {
+          res.sendStatus(200);
+        }
+      })
+      .catch((err) => console.log(err));
+  });
+
+  app.post("/isAdmin", (req, res) => {
+    const email = req.body.email;
+    AddAdminCollection.find({ email: email }).toArray((error, admins) => {
+      res.send(admins.length > 0);
+    });
+  });
+
+  //status update
+
+  app.patch("/updateStatus/:id", (req, res) => {
+    const id = req.params.id;
+    OrderCollection.updateOne(
+      { _id: ObjectId(id) },
+
+      { $set: { status: req.body.value } }
+    ).then((result) => res.send(result.modifiedCount > 0));
+  });
+});
+
+app.listen(process.env.PORT || port);
